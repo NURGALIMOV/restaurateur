e3e65d0c1fb28327ae2f4c756f1dac3726e35452
Ilshat Nurgalimov <il.nur86@mail.ru>
2021-11-14 10:03:44 +0300

    init project

---
+++ src/main/java/ru/inurgalimov/restaurateur/RestaurateurApplication.java
@@ -0,0 +1,13 @@
+package ru.inurgalimov.restaurateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurateurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurateurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/inurgalimov/restaurateur/RestaurateurApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.inurgalimov.restaurateur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurateurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
